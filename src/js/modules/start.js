eae8c002fb28b76b63ae14543919f8c0cc62182f
Natallia Shliapik <nshliapik@gmail,com>
2023-03-29 23:00:27 +0300

    start

--- src/js/modules/start.js
+++ src/js/modules/start.js
@@ -22,6 +22,7 @@ import { getMainPage } from './pages/mainPage.js';
 import { selfProductPageSlider } from '../modules/sliders/selfProductPageSlider.js';
 import { numberBtnMinusHandler } from './buttons/inputNumberButtons.js';
 import { numberBtnPlusHandler } from './buttons/inputNumberButtons.js';
+import { getParameterFromURL } from './getParameterFromURL.js';
 
 async function start() {
   const dataBase = await getDataBase();
@@ -50,11 +51,20 @@ async function start() {
       } else {
         category = decodeURIComponent(path.split('=')[1]);
       }
+      const dataForSelectedLocation = getDataForSelectedLocation(dataBase);
       page = getProductsPage(dataForSelectedLocation, productSpecificationData, '../../img/menuImg/productsImg/', category, parametr);
     } else if (routes[path] === 'selfProductPage') {
       page = getSelfProductPage(productSpecificationData[path.slice(1)], '../../img/menuImg/productsImg/', path.slice(1, path.length - 1), dataForSelectedLocation, productSpecificationData);
     } else {
-      getMainPage(dataForSelectedLocation, productSpecificationData, '../../img/menuImg/productsImg/');
+      const city = getParameterFromURL('city');
+      const address = getParameterFromURL('str');
+      const cityInSelect = document.querySelector(`[value=${city}]`);
+      cityInSelect.setAttribute('selected', true);
+
+      const addressInSelect = document.querySelector(`[value=${address}]`);
+      addressInSelect.setAttribute('selected', true);
+
+      page = getMainPage(dataForSelectedLocation, productSpecificationData, '../../img/menuImg/productsImg/');
     }
 
     renderReplace(root, page);
