8684e25ac8424aa6c3f3e3bd8ca7f33757fda8ca
Natallia Shliapik <nshliapik@gmail,com>
2023-07-18 17:30:35 +0300

    change button on sale slider card

--- src/js/partials/cards/getCardForSaleSlider.js
+++ src/js/partials/cards/getCardForSaleSlider.js
@@ -1,6 +1,6 @@
 import { getDOMElement } from '../../modules/getDOMElement.js';
 
-function getCardForSaleSlider({ name, imgName, price, currency, description, sale, newPrice }, imgPath) {
+function getCardForSaleSlider({ name, imgName, price, currency, description, sale, newPrice, id }, imgPath) {
   const saleCardContainer = getDOMElement('div', {
     className: 'container__saleSlider--card cardContainer text',
     style: `background-image: url(${imgPath + imgName})`,
@@ -30,7 +30,9 @@ function getCardForSaleSlider({ name, imgName, price, currency, description, sal
     priceContainer.appendChild(getDOMElement('p', { innerText: `${price} ${currency}`, className: 'cardContainer__price--regular text--bold' }));
   }
 
-  saleCardContainer.appendChild(getDOMElement('button', { innerText: 'Хочу!', type: 'button', className: 'cardContainer__submit text text--semi-bold text--l text--white ' }));
+  const getMoreInfButton = getDOMElement('a', { href: `/${encodeURIComponent(id)}`, innerText: 'Подробнее', className: 'cardContainer__submit text text--semi-bold text--l text--white ' });
+  getMoreInfButton.setAttribute('data-link', 'internal');
+  saleCardContainer.appendChild(getMoreInfButton);
 
   return saleCardContainer;
 }
