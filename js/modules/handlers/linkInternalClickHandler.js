4576e48a4391b8ca058384eddbc716c5bc4580ae
Natallia Shliapik <nshliapik@gmail,com>
2023-07-23 01:31:17 +0300

    test

--- js/modules/handlers/linkInternalClickHandler.js
+++ js/modules/handlers/linkInternalClickHandler.js
@@ -28,3 +28,7 @@ export const linkInternalClickHandler = (e) => {
     }
   }
 };
+
+/*'?p=/%D0%A1%D1%83%D1%88%D0%B8&q=city=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA~and~str=%D0%9F%D1%80%D0%B8%D1%82%D1%8B%D1%86%D0%BA%D0%BE%D0%B3%D0%BE%20100'
+
+"?city=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&str=%D0%9F%D1%80%D0%B8%D1%82%D1%8B%D1%86%D0%BA%D0%BE%D0%B3%D0%BE%20"*/
