54cb2d840b5893a5a195332e4e996d02f39acaf7
Natallia Shliapik <nshliapik@gmail,com>
2023-07-23 02:25:54 +0300

    test

--- js/modules/withCheckURL.js
+++ js/modules/withCheckURL.js
@@ -6,8 +6,10 @@ import { getParameterFromURL } from './getParameterFromURL.js';
 import { changeAddressSelectForm } from '../partials/header/changeAddressSelectForm.js';
 
 function withCheckURL(dataBase, productSpecificationData) {
-  const queryParam = window.location.search;
-  const path = window.location.pathname;
+  const initialQuery = window.location.search;
+
+  const queryParam = initialQuery.includes('?p=') ? initialQuery.replace(/.*q=/, '?') : initialQuery;
+  const path = initialQuery.includes('?p=') ? match(/(?:\?p=)(.*)(?=&q=)/)[1] : window.location.pathname;
   const root = '#rootCentral';
 
   if (!queryParam) {
@@ -30,3 +32,9 @@ function withCheckURL(dataBase, productSpecificationData) {
 }
 
 export { withCheckURL };
+
+/*'?p=/%D0%A1%D1%83%D1%88%D0%B8&q=city=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA~and~str=%D0%9F%D1%80%D0%B8%D1%82%D1%8B%D1%86%D0%BA%D0%BE%D0%B3%D0%BE%20100'
+
+"?city=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&str=%D0%9F%D1%80%D0%B8%D1%82%D1%8B%D1%86%D0%BA%D0%BE%D0%B3%D0%BE%20"
+
+(?:q=)(.*)*/
