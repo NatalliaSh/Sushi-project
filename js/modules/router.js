c9fb092a5eb1c733819cd08b85a99611b3f842b0
Natallia Shliapik <nshliapik@gmail,com>
2023-07-22 23:24:51 +0300

    test

--- js/modules/router.js
+++ js/modules/router.js
@@ -1,15 +1,15 @@
 function getRoutes(productSpecificationData, dataForSelectedLocation) {
   const listOfSelfProduct = Object.keys(productSpecificationData);
-  const listOfSelfProductPath = listOfSelfProduct.map((element) => 'Sushi-project/' + element);
+  const listOfSelfProductPath = listOfSelfProduct.map((element) => '/Sushi-project/' + element);
 
   const listOfCategories = dataForSelectedLocation.menuList;
-  const listOfCategoriesPath = listOfCategories.map((element) => 'Sushi-project/' + encodeURIComponent(element));
+  const listOfCategoriesPath = listOfCategories.map((element) => '/Sushi-project/' + encodeURIComponent(element));
 
   const routes = {};
   listOfSelfProductPath.forEach((element) => (routes[element] = 'selfProductPage'));
   listOfCategoriesPath.forEach((element) => (routes[element] = 'category'));
 
-  return routes; ////
+  return routes;
 }
 
 export { getRoutes };
